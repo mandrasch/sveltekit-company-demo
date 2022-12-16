4af5a890946f83266c8419fe4ae68b90647bfc1a
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-12-16 21:54:33 +0100

    chore: add TODO

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -1,5 +1,7 @@
 import { dev } from '$app/environment';
 
+// TODO: These values will change after we connected to an API for blogs posts
+
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
 export const csr = dev;
